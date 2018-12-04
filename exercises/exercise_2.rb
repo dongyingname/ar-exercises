d3ad36c71ca6525329b8d06dc6ae90186bfbbe5f
Ying Dong <dongyingname@yahoo.com>
2018-12-04 13:12:37 -0700

    Complete exercise2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.where(id: 1).first
+@store2 = Store.where(id: 2).first
+
+# puts "@store1's name",@store1['name']
+
+@store1['name'] = "Zhengzhou"
+@store1.save
+
+
