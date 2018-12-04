580f479e45db79e481f1c5155f12971b976111dd
Ying Dong <dongyingname@yahoo.com>
2018-12-04 15:28:40 -0700

    Validate store has greater than 0 revenue

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,13 @@
 class Store < ActiveRecord::Base
+  
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true,  numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  # validate :
   has_many :employees
+
+  
 end
 
