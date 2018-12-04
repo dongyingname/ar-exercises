3011c6cd8f9067fddc63cde05815fcc111e8fa50
Ying Dong <dongyingname@yahoo.com>
2018-12-04 13:47:45 -0700

    Finish exercise5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -6,5 +6,10 @@ require_relative './exercise_4'
 
 puts "Exercise 5"
 puts "----------"
+puts 'Exercise5:Total Annual Revenue,',Store.sum(:annual_revenue)
+puts "Average Annual Revenue of Each Store ", Store.sum(:annual_revenue) / Store.count
+
+num_stores_over_1m = Store.where('annual_revenue > ?', 1000000).count
+puts "Number of Stores that have over 1M Annual Revenue ", num_stores_over_1m
 
 # Your code goes here ...
